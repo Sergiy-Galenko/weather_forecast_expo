8e8df1bc0393968bc227f40150e489e8c5efd81b
Sergiy-Galenko <131354874+Sergiy-Galenko@users.noreply.github.com>
2023-05-09 14:54:27 +0300

    Update App.js new weather.js

--- weatherforecast/Loading/Loading.js
+++ weatherforecast/Loading/Loading.js
@@ -2,6 +2,7 @@ import React from "react";
 import { View, Text} from 'react-native'
 import {styles}  from './style';
 
+
 export default function Loading(){
     return (<View style = {styles.container}>
         <Text style = {styles.text}>Получение погоды...</Text>
---
+++ weatherforecast/Weather/weather.js
@@ -0,0 +1,10 @@
+import React from "react";
+import propTypes from 'prop-types'
+
+export default function Weather(){
+    return null;
+}
+
+Weather.propTypes = {
+    
+}
